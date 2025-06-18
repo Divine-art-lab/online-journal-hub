6d0049e0676e06bfd0607542f881d6947ef18ec6
Divine-art-lab <divineij35@gmail.com>
2025-06-18 11:25:23 +0100

    update

--- main.js
+++
@@ -1,9 +0,0 @@
-function display() {
-  // make show be the button
-  var show = document.getElementById("about");
-  if (show.style.display === "block") {
-    show.style.display = "none";
-  } else {
-    show.style.display = "block";
-  }
-}
